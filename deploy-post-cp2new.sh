e10f5bf99f22ac3e083b8070f1c105b27d40d29b
shmilee <shmilee.zju@gmail.com>
2022-08-17 01:45:51 +0800

    cp files after deploy

---
+++ deploy-post-cp2new.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright (C) 2022 shmilee
+src="${1}"
+dst="${2:-$(date +%F)-deploy}"
+if [ x"$src" == x"" ]; then
+    echo "usage: $0 <src> <dst>"
+    exit 1
+fi
+cp_files() {  # ${src}/xx/yy/zz*.zip -> ${dst}/xx/yy/
+    path="$1"    # xx/yy/ or ""
+    prefix="${2:-""}"  # zz or ""
+    suffix="${3:-""}"  # .zip or ""
+    for file in `ls -d "${src}/${path}${prefix}"*${suffix} 2>/dev/null`; do
+        cp -r -v -p "$file" "${dst}/${path}"
+    done
+}
+echo ' ==> backup logs'
+cp_files "" 'backup-log-' '.tar.gz'
+echo ' ==> v2ray zip'
+cp_files "" 'v2ray-linux-64-v' '.zip'
+echo ' ==> http-passwd'
+tail -n1 "$src"/etc/http-passwd >> "$dst"/etc/http-passwd
+tail -n1 "$src"/etc/http-passwd-jsproxy >> "$dst"/etc/http-passwd-jsproxy
+echo ' ==> ssl-certs'
+cp_files 'etc/ssl-certs/'
+echo ' ==> aria2 download files'
+cp_files "http/aria2-download/"
+echo ' ==> x2zl jsons'
+path='http/article/x2zl/jsons'
+[ -d "$dst"/$path ] || mkdir -pv "$dst"/$path
+cp_files "$path/"
