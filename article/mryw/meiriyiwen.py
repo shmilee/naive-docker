ccac8c5ae920ad2ad7274103cb71979a2ba6e332
shmilee <shmilee.zju@gmail.com>
2022-08-18 10:39:22 +0800

    add mryw index toc, use pypinyin to sort
    css ref: https://gitee.com/zhangxinxu/css-hr/blob/master/hr.css

--- article/mryw/meiriyiwen.py
+++ article/mryw/meiriyiwen.py
@@ -16,6 +16,10 @@ try:
     CCer = opencc.OpenCC('t2s.json')
 except:
     CCer = None
+try:
+    import pypinyin
+except:
+    pypinyin = None
 
 URL = 'https://interface.meiriyiwen.com/article/day?dev=1&date='
 
@@ -81,7 +85,7 @@ def main(output, start=(2011, 3, 8), stop=(2020, 9, 9), dt=3):
                 break
     if results_404:
         print('[Info] %d empty(40404) days.' % len(results_404))
-        #print('\t', results_404)
+        # print('\t', results_404)
     if results:
         results.update(old_results)
         with open(output, 'w', encoding='utf8') as out:
@@ -90,7 +94,7 @@ def main(output, start=(2011, 3, 8), stop=(2020, 9, 9), dt=3):
             json.dump(results, out, ensure_ascii=False)
 
 
-def split_json(file, outdir='./split-jsons'):
+def split_json(file, outdir='./split-jsons', uri='/article?key='):
     '''Read results in source file, save them to outdir/{date,title}-names.'''
     if not os.path.exists(file):
         print("[Error] %s not found!" % file)
@@ -122,9 +126,9 @@ def split_json(file, outdir='./split-jsons'):
                     sr = difflib.SequenceMatcher(None, dig, digest).ratio()
                     # 0.571 0.714 0.857 0.885 , >0.9
                     if sr > 0.9:
-                        #print('%s %s' % (sr, title))
-                        #print(alias[title][dig], dig)
-                        #print([day], digest, '\n')
+                        # print('%s %s' % (sr, title))
+                        # print(alias[title][dig], dig)
+                        # print([day], digest, '\n')
                         alias[title][dig].append(day)  # similar-add
                         added = True
                         break
@@ -144,7 +148,7 @@ def split_json(file, outdir='./split-jsons'):
             print("[Info] %s exists." % output)
             return
         with open(output, 'w', encoding='utf8') as out:
-            #print("[Info] Writting %s ..." % output)
+            # print("[Info] Writting %s ..." % output)
             json.dump(dict(data=results[day]), out, ensure_ascii=False)
         u_dates.append(day)
         l_dates.extend(daylinks)
@@ -155,8 +159,27 @@ def split_json(file, outdir='./split-jsons'):
                 print("[Info] %s exists." % link)
             else:
                 src = os.path.basename(output)  # same dir with *link*
-                #print("[Info] Linking %s -> %s ..." % (link, src))
+                # print("[Info] Linking %s -> %s ..." % (link, src))
                 os.symlink(src, link)
+
+    idx_info = []
+
+    def _make_content_li(title, days):
+        atmp = "<a href='{u}{k}' target='_blank'>{k}</a>"
+        ai = [atmp.format(u=uri, k=k) for k in days + [title]]
+        ai.insert(1, ':')
+        at = [atmp.format(u=uri, k=k) for k in [title] + days]
+        at.insert(1, ':')
+        litmp = '<li><p>{a}</p></li>'
+        if pypinyin:
+            tpy = pypinyin.pinyin(title, style=pypinyin.Style.TONE3)
+            py1 = pypinyin.pinyin(title, style=pypinyin.Style.NORMAL)
+            title = ''.join([z[0] for z in tpy if z[0] not in '“”（）()'])
+            py1 = py1[0][0] if py1[0][0] not in '“”（）()' else py1[1][0]
+        else:
+            py1 = title[0] if title[0] not in '“”（）()' else title[1]
+        idx_info.append((days[0], litmp.format(a=''.join(ai)),
+                         title, py1, litmp.format(a=''.join(at))))
 
     for title, ds in alias.items():
         # sort -> [[days-1], ...]
@@ -174,18 +197,58 @@ def split_json(file, outdir='./split-jsons'):
                 tlinks = ['%s-%d' % (title, i)]
                 if i == 1:
                     tlinks.append(title)
+                    _make_content_li(title, days + [tlinks[0]])
+                else:
+                    _make_content_li(tlinks[0], days)
                 _write_data(days[0], days[1:], tlinks)
         else:
             # title, [days]
             days = dsl[0]
             _write_data(days[0], days[1:], [title])
+            _make_content_li(title, days)
+
+    # index, toc
+    hr = '<hr class="hr-fade-content" data-content="{grp}">'
+    idx_info.sort(key=lambda i: i[0])
+    grp = idx_info[0][0][:-2]  # 201103
+    index = [hr.format(grp='%s-%s' % (grp[:4], grp[4:]))]
+    for i in idx_info:
+        if grp == i[0][:-2]:
+            index.append(i[1])
+        else:
+            grp = i[0][:-2]
+            index.extend([hr.format(grp='%s-%s' % (grp[:4], grp[4:])), i[1]])
+    index = '\n'.join(index)
+    idx_info.sort(key=lambda i: i[2])
+    # grp idx_info[j][3] # 1,2,5,P,S; a; ai; ...
+    grp = ['18', '1905', '1964-', '2.05', '2018', '51', 'PEPA', 'SOS-']
+    toc = [hr.format(grp=', '.join(sorted(set(g[0] for g in grp))))]
+    for i in idx_info:
+        if i[3] in grp:
+            toc.append(i[4])
+        else:
+            grp = [i[3]]
+            toc.extend([hr.format(grp=grp[0]), i[4]])
+    toc = '\n'.join(toc)
+    ctmp = '<div id="toc"><ol>{c}</ol></div>'
+    data_d = dict(
+        date=dict(prev="20220816", curr="20220817", nect="20220818"),
+        title="索引", author="mryw", digest="None", wc=len(index),
+        content=ctmp.format(c=index),
+    )
+    print("[Info] Writting keys: 'index' 'toc' ...")
+    with open('%s/index.json' % outdir, 'w', encoding='utf8') as out:
+        json.dump(dict(data=data_d), out, ensure_ascii=False)
+    data_d.update(content=ctmp.format(c=toc), title="目录", wc=len(toc))
+    with open('%s/toc.json' % outdir, 'w', encoding='utf8') as out:
+        json.dump(dict(data=data_d), out, ensure_ascii=False)
 
     keys = u_dates + l_dates + l_titles
     with open('%s/keys.json' % outdir, 'w', encoding='utf8') as out:
         print("[Info] Writting (%d/%d) dates ..." % (len(u_dates), L))
         print("[Info] Writting (%d/%d) link-dates ..." % (len(l_dates), L))
         print("[Info] Writting (%d/%d) link-titles ..." % (len(l_titles), L))
-        json.dump(sorted(keys), out, ensure_ascii=False)
+        json.dump(['index', 'toc'] + sorted(keys), out, ensure_ascii=False)
 
 
 if __name__ == '__main__':
