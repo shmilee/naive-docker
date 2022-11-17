93781f722dbee7a1732785e2ef7dab3e35cce424
shmilee <shmilee.zju@gmail.com>
2022-11-17 20:36:54 +0800

    install docker in debian 11

--- pre-docker-debian11.sh
+++ pre-docker-debian11.sh
@@ -3,10 +3,10 @@
 echo "==> add docker-ce"
 apt-get update
 apt-get install ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-cache policy docker-ce
 apt-get install docker-ce
